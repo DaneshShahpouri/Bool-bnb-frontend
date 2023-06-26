26c09cab0b38f755200d33f540c5119ddaecfb1e
enricoderasmo21 <derasmo.enrico@gmail.com>
2023-06-26 18:57:32 +0200

    fix: style in AppShow and AppMessageForm

--- src/components/AppMessageForm.vue
+++ src/components/AppMessageForm.vue
@@ -68,38 +68,47 @@ export default {
 }
 </script>
 <template>
-    <div id="appMessageForm" class="my-5">
-        <h4>Send Message</h4>
-        <form @submit.prevent="sendMessage()">
+    <div class="_container">
 
-            <!-- username -->
-            <div class="mb3 ">
-                <label class="mx-3" for="username">username</label>
-                <input type="text" name="username" id="username" minlength="3" maxlength="100" class="form-control my-label"
-                    v-model="this.store.message_username" required>
+        <div id="appMessageForm" class="my-5">
+            <div class="title-container">
+                <!-- <i class="fa-solid fa-envelope icon"></i> -->
+                <i class="fa-regular fa-envelope icon"></i>
+                <div class="title">Send a message</div>
             </div>
-
-            <!-- content -->
-            <div class="mb3">
-                <label class="mx-3" for="content">content</label>
-                <textarea type="text" name="content" id="content" class="form-control my-label"
-                    v-model="this.store.message_content" required minlength="5" maxlength="300"></textarea>
-            </div>
-
-            <!-- email -->
-            <div class="mb3">
-                <label class="mx-3" for="email">email</label>
-                <input type="email" name="email" id="email" class="form-control my-label" v-model="this.store.message_email"
-                    required minlength="5" maxlength="100">
-            </div>
-
-            <!-- submit -->
-            <div class="mb-3 d-flex justify-content-center">
-                <button class="btn btn-outline-primary my-3" type="submit">Send</button>
-            </div>
-
-        </form>
-
+            <form @submit.prevent="sendMessage()">
+    
+                <!-- username -->
+                <div class="mb-1">
+                    <label class="mx-1" for="username">Username</label>
+                    <input type="text" name="username" id="username" minlength="3" maxlength="100" class="form-control my-label"
+                        v-model="this.store.message_username" required>
+                </div>
+    
+                <!-- content -->
+                <div class="mb-1">
+                    <label class="mx-1" for="content">Content</label>
+                    <textarea type="text" name="content" id="content" class="form-control my-label"
+                        v-model="this.store.message_content" required minlength="5" maxlength="300"></textarea>
+                </div>
+    
+                <!-- email -->
+                <div class="mb-3">
+                    <label class="mx-1" for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-control my-label" v-model="this.store.message_email"
+                        required minlength="5" maxlength="100">
+                </div>
+    
+                <!-- submit -->
+                <div class="d-flex justify-content-center">
+                    <button class="btn my-3 _btn-send" type="submit">
+                        <i class="fa-solid fa-paper-plane"></i>
+                    </button>
+                </div>
+    
+            </form>
+    
+        </div>
         <div class="alert" :class="(this.isSend ? 'alert-success' : (this.isError ? 'alert-danger' : ''))" role="alert">
             {{ this.isSend ? 'Success Request' : (this.isError ? this.error : '') }}
         </div>
@@ -107,10 +116,54 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-#appMessageForm {
+
+.container{
+    display: flex;
+    flex-direction: row;
+
     width: 30%;
-    border: 1px solid lightgray;
-    border-radius: 7px;
-    padding: 1.5em;
+    
+    #appMessageForm {
+        
+        border: 1px solid lightgray;
+        border-radius: 7px;
+        padding: 1.5em;
+        padding-bottom: 0;
+    
+        box-shadow: 0px 6px 20px rgba($color: #000000, $alpha: 0.2);
+    
+        .title-container{
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+    
+            margin-bottom: 14px;
+    
+            .icon{
+                font-size: 3em;
+    
+                color: #ff5f64;
+            }
+    
+            .title{
+                font-size: 1.5em;
+                font-weight: bold;
+            }
+        }
+    
+        ._btn-send{
+            padding: 8px 26px;
+            border: 2px solid #ff5f64;
+            border-radius: 10px;
+            color: #ff5f64;
+    
+            &:hover{
+                background-color: #ff5f64;
+                color: white;
+            }
+        }
+    }
 }
 </style>
--- src/pages/AppShowApartment.vue
+++ src/pages/AppShowApartment.vue
@@ -79,7 +79,7 @@ export default {
         <div class="container d-flex align-items-start">
             <div class="mainSide m-5">
 
-                <div class="card">
+                <div class="card _card">
                     <div class="img-wrapper">
                         <img class="card-img-top"
                             :src="(apartment.cover_image != null && (apartment.cover_image.slice(apartment.cover_image.length - 3, apartment.cover_image.length) == 'png' || apartment.cover_image.slice(apartment.cover_image.length - 3, apartment.cover_image.length) == 'jpg' || apartment.cover_image.slice(apartment.cover_image.length - 3, apartment.cover_image.length) == 'ebp' || apartment.cover_image.slice(apartment.cover_image.length - 3, apartment.cover_image.length) == 'peg') ? (this.store.urlImg + apartment.cover_image) : 'https://www.kuleuven.be/communicatie/congresbureau/fotos-en-afbeeldingen/no-image.png/image')"
@@ -87,27 +87,39 @@ export default {
                     </div>
                     <div class="card-body">
                         <h3>{{ apartment.name }}</h3>
-                        <p class="card-text">{{ apartment.description }}</p>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item"> Address: <strong>{{ apartment.address }}</strong></li>
-                        <li class="list-group-item"> Rooms: <strong>{{ apartment.rooms_number }}</strong></li>
-                        <li class="list-group-item"> Beds: <strong>{{ apartment.beds_number }}</strong></li>
-                        <li class="list-group-item"> Bathrooms: <strong>{{ apartment.bathrooms_number }}</strong></li>
+                        <li class="list-group-item"> <i class="fa-solid fa-door-closed"></i> Rooms: <strong>{{ apartment.rooms_number }}</strong></li>
+                        <li class="list-group-item"> <i class="fa-solid fa-bed"></i> Beds: <strong>{{ apartment.beds_number }}</strong></li>
+                        <li class="list-group-item"> <i class="fa-solid fa-bath"></i> Bathrooms: <strong>{{ apartment.bathrooms_number }}</strong></li>
                         <li class="list-group-item"> Area (sqm): <strong>{{ apartment.sqm }}</strong></li>
-                        <li class="list-group-item d-flex gap-4 flex-wrap"> Amenities:
-                            <div v-for="service in apartment.services"
-                                class="d-flex justify-content-center align-items-center gap-2 flex-nowrap">
-                                <span>{{ service.name }}</span>
-                                <span v-html="service.icon"></span>
-                            </div>
-                        </li>
-                        <div></div>
                     </ul>
                 </div>
 
-                <div class="d-flex justify-content-center my-5">
-                    <router-link :to="{ name: 'home' }" class="btn btn-outline-primary">See Others</router-link>
+                <div class="title">Description</div>
+
+                <p class="desc margin">{{ apartment.description }}</p>
+
+                <hr class="margin">
+
+                <div class="title">What you'll find</div>
+
+                <div class="amenities-container">
+                    
+                    <div v-for="service in apartment.services" class="inner-container">
+                        <span class="icon" v-html="service.icon"></span>
+                        <span class="am-name">{{ service.name }}</span>
+                    </div>
+
+                </div>
+                
+                <div class="address-container">
+                    <hr class="margin last">
+                    
+                    <div class="address"> 
+                        <i class="fa-solid fa-location-dot icon"></i> 
+                        <strong>{{ apartment.address }}</strong>
+                    </div>
                 </div>
 
             </div>
@@ -116,15 +128,29 @@ export default {
         </div>
 
         <div class="container" style="height: 500px;">
-            <div id="map" style="width: 100% ; height: 100%; "></div>
+            <div id="map" class="map" style="width: 100% ; height: 100%;"></div>
         </div>
-
+        
+        <div class="d-flex justify-content-center my-5">
+            <router-link :to="{ name: 'home' }" class="btn _btn-see">See Others</router-link>
+        </div>
+       
     </div>
 </template>
 
 <style lang="scss" scoped>
 .mainSide {
     width: 70%;
+
+    .margin{
+        margin-bottom: 40px;
+    }
+
+    
+    ._card{
+
+        margin-bottom: 30px;
+    }
 
     .img-wrapper {
         height: 400px;
@@ -135,6 +161,83 @@ export default {
             height: 100%;
             object-fit: cover;
         }
+    }
+
+    .title{
+        margin-bottom: 40px;
+
+        font-size: 2em;
+        // font-weight: bold;
+    }
+
+    .desc{
+        font-size: 1.2em;
+    }
+
+    .amenities-container{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: center;
+        gap: 60px;
+
+        margin-bottom: 60px;
+        padding-left: 40px;
+
+        .inner-container{
+            display: flex;
+            gap: 10px;
+            align-items: center;
+
+            width: 200px;
+
+            .icon{
+                font-size: 2em;
+                color: #ff5f64;
+            }
+    
+            .am-name{
+                font-size: 1.2em;
+            }
+        }
+
+    }
+
+    .address-container{
+
+        .icon{
+            font-size: 1.4em;
+            color: #ff5f64;
+        }
+
+        .address{
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            font-size: 1.3em;
+        }
+    }
+
+}
+
+.last{
+    width: 1200px;
+}
+
+.map{
+    box-shadow: 0px 10px 20px rgba($color: #000000, $alpha: 0.4);
+}
+._btn-see{
+    padding: 8px 26px;
+    border: 2px solid #ff5f64;
+    border-radius: 10px;
+    color: #ff5f64;
+
+    font-weight: bold;
+
+    &:hover{
+        background-color: #ff5f64;
+        color: white;
     }
 }
 </style>
