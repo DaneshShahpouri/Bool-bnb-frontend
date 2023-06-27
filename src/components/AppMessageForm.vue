da0268e2949f09c10574a14684fd3973c6892406
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-06-27 11:01:22 +0200

    feat: added required fields in message form

--- src/components/AppMessageForm.vue
+++ src/components/AppMessageForm.vue
@@ -77,37 +77,37 @@ export default {
                 <div class="title">Send a message</div>
             </div>
             <form @submit.prevent="sendMessage()">
-    
+
                 <!-- username -->
                 <div class="mb-1">
-                    <label class="mx-1" for="username">Username</label>
-                    <input type="text" name="username" id="username" minlength="3" maxlength="100" class="form-control my-label"
-                        v-model="this.store.message_username" required>
+                    <label class="mx-1" for="username">Username*</label>
+                    <input type="text" name="username" id="username" minlength="3" maxlength="100"
+                        class="form-control my-label" v-model="this.store.message_username" required>
                 </div>
-    
+
                 <!-- content -->
                 <div class="mb-1">
-                    <label class="mx-1" for="content">Content</label>
+                    <label class="mx-1" for="content">Content*</label>
                     <textarea type="text" name="content" id="content" class="form-control my-label"
                         v-model="this.store.message_content" required minlength="5" maxlength="300"></textarea>
                 </div>
-    
+
                 <!-- email -->
                 <div class="mb-3">
-                    <label class="mx-1" for="email">Email</label>
-                    <input type="email" name="email" id="email" class="form-control my-label" v-model="this.store.message_email"
-                        required minlength="5" maxlength="100">
+                    <label class="mx-1" for="email">Email*</label>
+                    <input type="email" name="email" id="email" class="form-control my-label"
+                        v-model="this.store.message_email" required minlength="5" maxlength="100">
                 </div>
-    
+
                 <!-- submit -->
                 <div class="d-flex justify-content-center">
                     <button class="btn my-3 _btn-send" type="submit">
                         <i class="fa-solid fa-paper-plane"></i>
                     </button>
                 </div>
-    
+
             </form>
-    
+
         </div>
         <div class="alert" :class="(this.isSend ? 'alert-success' : (this.isError ? 'alert-danger' : ''))" role="alert">
             {{ this.isSend ? 'Success Request' : (this.isError ? this.error : '') }}
@@ -116,50 +116,49 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-
-.container{
+.container {
     display: flex;
     flex-direction: row;
 
     width: 30%;
-    
+
     #appMessageForm {
-        
+
         border: 1px solid lightgray;
         border-radius: 7px;
         padding: 1.5em;
         padding-bottom: 0;
-    
+
         box-shadow: 0px 6px 20px rgba($color: #000000, $alpha: 0.2);
-    
-        .title-container{
+
+        .title-container {
             display: flex;
             flex-direction: row;
             justify-content: center;
             align-items: center;
             gap: 10px;
-    
+
             margin-bottom: 14px;
-    
-            .icon{
+
+            .icon {
                 font-size: 3em;
-    
+
                 color: #ff5f64;
             }
-    
-            .title{
+
+            .title {
                 font-size: 1.5em;
                 font-weight: bold;
             }
         }
-    
-        ._btn-send{
+
+        ._btn-send {
             padding: 8px 26px;
             border: 2px solid #ff5f64;
             border-radius: 10px;
             color: #ff5f64;
-    
-            &:hover{
+
+            &:hover {
                 background-color: #ff5f64;
                 color: white;
             }
